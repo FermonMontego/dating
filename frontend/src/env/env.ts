314b117727cfdf9e1690f198344156b8096b124d
FermonMontego <fermonmantego@gmail.com>
2024-10-01 16:16:48 +0700

    fix: env to js file

--- frontend/src/env/env.ts
+++ frontend/src/env/env.ts
@@ -1,4 +1,9 @@
-export const env = {
+type T_ENV = {
+  base_dev_url: string;
+  production_api_uri: string
+}
+
+export const env: T_ENV = {
   base_dev_url: 'http://localhost:8081/api',
   production_api_uri: `${window.location.origin}/api`
 };
