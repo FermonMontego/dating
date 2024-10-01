1bcae03c01f5de31e18fd0a15155da47223cb268
FermonMontego <fermonmantego@gmail.com>
2024-10-01 15:30:15 +0700

    add pg to sequelize

--- backend/db/database.js
+++ backend/db/database.js
@@ -1,8 +1,10 @@
 import { Sequelize } from 'sequelize';
+import pg from 'pg';
 
 const sequelize = await new Sequelize(process.env.PG_DB, process.env.PG_USER, process.env.PG_PASSWORD, {
     host: process.env.PG_HOST,
-    dialect: 'postgres'
+    dialect: 'postgres',
+    dialectModule: pg
 });
 
 export default sequelize;
