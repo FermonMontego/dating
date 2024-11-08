9798f19a30f213b1465649cef66349797a3738f8
FermonMontego <fermonmantego@gmail.com>
2024-11-09 00:17:15 +0700

    add toast

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { ChakraProvider } from '@chakra-ui/react';
 import { RouterProvider } from 'react-router-dom';
 
 import { router } from './routes/router';
 
+import 'react-toastify/ReactToastify.css';
+import { ToastContainer, toast } from 'react-toastify';
+
 const App = () => {
+  useEffect(() => {
+    toast('Toast app', {
+      type: 'error',
+      position: 'bottom-right',
+    });
+  }, []);
   return (
     <ChakraProvider>
       <RouterProvider router={router}></RouterProvider>
+      <ToastContainer />
     </ChakraProvider>
   );
 };
